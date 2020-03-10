0d2ebd15ae95b4a0524de263a61ddb1a3ae3431b
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-03-10 11:13:16 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,7 +2,7 @@ class Backer
   attr_reader :name, :title
   attr_accessor :backed_projects
 
-  def initialize(name)
+  def initialize(name, title)
     @name = name
     @backed_projects =[]
   end
